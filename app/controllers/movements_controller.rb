ff0edfe9fb3730e294ae72c01ad6d5e17062b8e2
github username apalski <github email address annette.drapalski@gmail.com>
2017-05-29 18:17:11 +1000

    Minor alignment movement, wod & user controllers and stubout movement controller

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,2 +1,46 @@
 class MovementsController < ApplicationController
+
+	def index
+		@movements = Movement.all
+	end
+
+	def new
+		@movement = Movement.new
+	end
+
+	def create
+		@movement = Movement.new(movement_params)
+		if @movement.save
+			redirect_to movement_path(@movement)
+		else
+			render :new
+		end		
+	end
+
+	def show
+		set_movement
+	end
+
+	def edit
+		set_movement
+		if @movement.update(movement_params)
+			redirect_to movement_path(@movement)
+		else
+			render :edit
+		end		
+	end
+
+	def update
+		set_movement
+	end
+
+	private
+
+	def set_movement
+		@movement = Movement.find(params[:id])
+	end
+
+	def movement_params
+		params.require(:movement).permit(:name, :date, :result, :type, :pr)
+	end
 end
--- app/controllers/wods_controller.rb
+++ app/controllers/wods_controller.rb
@@ -1,2 +1,7 @@
 class WodsController < ApplicationController
+
+
+	
 end
+
+
