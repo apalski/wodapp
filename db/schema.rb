af60bcfdda5fddeb6e54156d4c5133d952a0cd80
github username apalski <github email address annette.drapalski@gmail.com>
2017-06-08 15:14:05 +1000

    Dropped and re-migrated db, altered views for new userwods and usermovements to draw name from Movement and Wod

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529044453) do
+ActiveRecord::Schema.define(version: 20170608045642) do
 
   create_table "move_wods", force: :cascade do |t|
     t.integer  "movement_id"
@@ -20,22 +20,29 @@ ActiveRecord::Schema.define(version: 20170529044453) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "movements", force: :cascade do |t|
+    t.string   "name"
+    t.string   "movement_type"
+    t.integer  "owner_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
   create_table "usermovements", force: :cascade do |t|
     t.string   "name"
     t.date     "date"
-    t.string   "movement_type"
     t.float    "result"
-    t.boolean  "pr",            default: false
+    t.boolean  "pr",         default: false
     t.integer  "user_id"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.boolean  "admin",           default: false
+    t.boolean  "owner",           default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
   end
@@ -43,12 +50,19 @@ ActiveRecord::Schema.define(version: 20170529044453) do
   create_table "userwods", force: :cascade do |t|
     t.string   "name"
     t.date     "date"
-    t.string   "wod_type"
     t.float    "result"
     t.boolean  "pr",         default: false
     t.integer  "user_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "wods", force: :cascade do |t|
+    t.string   "name"
+    t.string   "wod_type"
+    t.integer  "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
