6515832ba30d1be0da515ecadaf832fb11001e41
github username apalski <github email address annette.drapalski@gmail.com>
2017-06-18 22:23:37 +1000

    Fine tune usermovements controller to ensure owner access only to all actions

--- app/controllers/usermovements_controller.rb
+++ app/controllers/usermovements_controller.rb
@@ -1,26 +1,17 @@
 class UsermovementsController < ApplicationController
 
 	def index
-		if params[:user_id]
-			set_user
-			if params[:user_id] == current_user.id.to_s
-				@usermovements = User.find(params[:user_id]).usermovements
-			else
-				redirect_to user_path(current_user), notice: "No movements to display"	
-			end	
+		if params[:user_id] == current_user.id.to_s
+			@usermovements = User.find_by(id: params[:user_id]).usermovements
 		else
-			redirect_to user_path(current_user), notice: "No movements to display"
+			redirect_to user_usermovements_path(current_user), notice: "Movement not found"	
 		end		
 	end
 
 	def new
-		if params[:user_id]
-			set_user
 			@usermovement = Usermovement.new(user_id: params[:user_id])
+			@user = current_user
 			set_movement
-		else
-			redirect_to users_path, notice: "User not found"
-		end		
 	end
 
 	def create
@@ -39,40 +30,30 @@ class UsermovementsController < ApplicationController
 	end
 
 	def show
-		if params[:user_id]
-			set_user
-			if current_user.id.to_s != params[:user_id]
-				redirect_to user_usermovements_path(@user), notice: "Movement not found"
-			else
+		if current_user.id.to_s == params[:user_id]
+			if set_usermovement && set_usermovement.user_id == current_user.id
 				set_usermovement
-			end		
-		else
-			set_usermovement
+			else
+				redirect_to user_usermovements_path(current_user), notice: "Movement not found"
+			end	
+		else	
+			redirect_to user_usermovements_path(current_user), notice: "Movement not found"
 		end			
 	end
 
 	def edit
-		if params[:user_id]
-			set_user
-			if @user.nil?
-				redirect_to users_path, notice: "User not found"
-			elsif @usermovement && @user.id == @usermovement.user_id
-				set_usermovement
-				set_movement
-			else
-				redirect_to user_usermovements_path(@user), notice: "Movement not found"	
-			end
-		else
+		if current_user.id == set_usermovement.user_id
+			set_usermovement
+			@user = current_user
 			set_movement
-		end	
+		else
+			redirect_to user_usermovements_path(current_user), notice: "Movement not found"	
+		end
 	end
 
 	def update
 		set_usermovement
 		if @usermovement.update(usermovement_params)
-			if @usermovement.pr == true
-				update_pr(@usermovement)
-			end	
 			redirect_to user_usermovement_path(current_user, @usermovement)
 		else
 			render :edit
@@ -80,19 +61,14 @@ class UsermovementsController < ApplicationController
 	end
 
 	def destroy
-		if params[:user_id]
-			set_user
-			if params[:user_id] == current_user.id.to_s
-				set_usermovement.destroy
-				respond_to do |format|
-					format.html {redirect_to user_usermovements_path(current_user), notice: "Movement was successfully deleted"}
-				end
-			else 
-				redirect_to	redirect_to user_usermovements_path(@user), notice: "Movement not found"
+		if params[:user_id] == current_user.id.to_s
+			set_usermovement.destroy
+			respond_to do |format|
+				format.html {redirect_to user_usermovements_path(current_user), notice: "Movement was successfully deleted"}
 			end
-		else
-			redirect_to user_usermovements_path(@user), notice: "Movement not found"
-		end			
+		else 
+			redirect_to	redirect_to user_usermovements_path(@user), notice: "Movement not found"
+		end	
 	end
 
 	private
@@ -101,17 +77,9 @@ class UsermovementsController < ApplicationController
 		@movements = Movement.all
 	end
 
-	def set_user
-		@user = User.find_by(id: params[:user_id])
-	end
-
 	def set_usermovement
 		@usermovement = Usermovement.find_by(id: params[:id])
 	end	
-
-	def set_user
-		@user = current_user
-	end
 
 	def usermovement_params
 		params.require(:usermovement).permit(:name, :date, :result, :pr, :user_id)
