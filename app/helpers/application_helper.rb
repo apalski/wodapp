3d0b6d07d6614cf1e80dbbb50659971e72a660c9
github username apalski <github email address annette.drapalski@gmail.com>
2017-06-18 18:06:26 +1000

    Edit #update_pr in application_helper.rb to isolate pr update to a specific movement or wod name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
 		@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
 	end
 
-	def update_pr(usermovement)
-		moves = Usermovement.select {|move| move.pr == true}
+	def update_pr(newpr)
+		moves = Usermovement.select {|move| move.pr == true if newpr.name == move.name}
 		if moves.size > 1
 			moves[0].pr = false
 			moves[0].save
