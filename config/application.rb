95df05b6b4a1bb17d07e8839a31ec0846ada9652
github username apalski <github email address annette.drapalski@gmail.com>
2017-06-23 13:55:24 +1000

    Add ruby 2.3.1 to gemfile, alter references to flatiron store to wodapp in application.rb, database.yml and session_store.rb

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsFlatironStore
+module Wodapp
   class Application < Rails::Application
 
     config.generators do |g|
