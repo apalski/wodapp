845ab5be156f8796a9539746d944dabdc3f3d851
github username apalski <github email address annette.drapalski@gmail.com>
2017-06-24 11:49:52 +1000

    Switching db from sqlite3 to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170608045642) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "move_wods", force: :cascade do |t|
     t.integer  "movement_id"
     t.integer  "wod_id"
