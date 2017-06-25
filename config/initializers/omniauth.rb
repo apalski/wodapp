d1e42f2574ba2fd018ae8336d93cbdaa7466d11e
github username apalski <github email address annette.drapalski@gmail.com>
2017-06-25 16:18:54 +1000

    Add Facebook to omniauth in navigation view and omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, '0baa007762dc36d143ea', 'bf581cc5dc724d2a4302cc36e5132b2749e76ccb'
+  provider :facebook, '674186382773850', 'd1e387722b64bde1f51177d316ad6f08'
 end
