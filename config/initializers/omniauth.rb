c53c8be27a7ea42d5fe565d8762a9a8f16e7dcbb
github username apalski <github email address annette.drapalski@gmail.com>
2017-06-25 17:23:21 +1000

    Edited omniauth.rb to correct typo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,6 @@
 
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
- provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: ‘public_profile’, info_fields: ‘id,name,link’
- provider :github, Rails.application.secrets.github_jey, Rails.application.secrets.github_secret, scope: ‘public_profile’, info_fields: ‘id,name,link’
+ provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: ‘public_profile’, info_fields: ‘id,name,link’,
+ provider :github, Rails.application.secrets.github_key, Rails.application.secrets.github_secret, scope: ‘public_profile’, info_fields: ‘id,name,link’
 end
