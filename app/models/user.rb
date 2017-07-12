8fe51ee26ab22812cfe34e9fed44aff9128dedc9
github username apalski <github email address annette.drapalski@gmail.com>
2017-07-12 11:35:09 +1000

    Alter random email for omniauth user to actual email

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
 	# Creates a new Facebook or Github user if they don't already exist
 	def self.create_with_omniauth(auth_hash)
 	    user = User.find_or_create_by(uid: auth_hash["uid"])
-	    user.email = "#{auth_hash["uid"]}@#{auth_hash["provider"]}.com"
+	    user.email = auth_hash["info"]["email"]
 	    user.password = SecureRandom.base64(10)
 	    user.name = auth_hash["info"]["name"]
 	    if User.exists?(user)
